65f38ac0fc7b2e49fc0790c105b062da49109683
vlindenbr <vlinden@tca.com.br>
2025-08-27 14:50:44 -0300

    fix: index.js fix to test lint

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-let didShowMessage = false;
-
 function showMessage(){
     const message ='Testing ESLINT'
 
     for (let i = 0; i < 5; i++){
+        // eslint-disable-next-line no-conole
         console.log(message);
     }
 }
