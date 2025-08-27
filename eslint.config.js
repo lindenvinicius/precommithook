2a4e0f65f254ed89e05552eff43b9476a3ebaa65
vlindenbr <vlinden@tca.com.br>
2025-08-27 14:46:25 -0300

    fix: eslint config file pure JS

--- eslint.config.js
+++ eslint.config.js
@@ -1,42 +1,17 @@
 // eslint.config.js
 import js from "@eslint/js";
-import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  // 1) Global ignores (CI & local)
-  {
-    ignores: ["dist/**", "build/**", "node_modules/**", "*.min.js"],
-  },
-
-  // 2) Base JS rules
+export default [
+  { ignores: ["dist/**", "build/**", "node_modules/**", "*.min.js"] },
   js.configs.recommended,
-
-  // 3) TS recommended rules
-  ...tseslint.configs.recommended,
-
-  // 4) Your project files
-  {
-    files: ["**/*.ts", "**/*.tsx"],
-    languageOptions: {
-      parserOptions: {
-        // If you have a tsconfig at a subpath, point to it
-        projectService: true, // auto-detects tsconfigs; use this OR set `project: "./tsconfig.json"`
-        ecmaVersion: "latest",
-        sourceType: "module",
-      },
-    },
-    rules: {
-      // tighten/relax as you like
-      "no-unused-vars": "off", // use TS rule instead
-      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-    },
-  },
-
-  // 5) JS/TS common files
   {
     files: ["**/*.js", "**/*.jsx"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
     rules: {
       "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     },
-  }
-);
+  },
+];
